8e41d7b4b0ae56dd69f95a9957e2b7f69f93f995
Filip Turek <filip.turek@a4bee.com>
2025-07-09 13:24:56 +0200

    Hello to the object orientation

---
+++ ClassesObjectsMethods/Course/HelloWorld/src/Main.java
@@ -0,0 +1,17 @@
+import model.*;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Person tom = new Person("Tom", "Smith", LocalDate.of(1984, 6, 15));
+        Person janet = new Person("Janet", "Jackson", LocalDate.of(1985,12,3));
+
+        tom.setSpouse(janet);
+        Dog fido = new Dog("Fido", LocalDate.of(2018,1,1));
+
+        tom.setPet(fido);
+        System.out.println(tom);
+    }
+}
---
+++ ClassesObjectsMethods/Course/HelloWorld/src/model/Dog.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Dog extends Pet{
+
+    public Dog(String name, LocalDate dob) {
+        super(name, dob);
+    }
+
+    public void bark() {
+        System.out.println("Ruff ruff!");
+    }
+}
